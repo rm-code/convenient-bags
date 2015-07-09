53c26c2bbbc2e4c7286c01d45a4410a6e4db252f
Robert Machmer <robert.machmer@gmail.com>
2015-07-09 11:25:21 +0200

    Use luautils.okModal instead of the local showOkModal

--- media/lua/client/UI/UIUnpackBagsMenu.lua
+++ media/lua/client/UI/UIUnpackBagsMenu.lua
@@ -1,5 +1,6 @@
 require('Mods/UnpackBags/TimedActions/TAUnpackBag');
 require('TimedActions/ISTimedActionQueue');
+require('luautils');
 
 -- ------------------------------------------------
 -- Constants
@@ -17,34 +18,6 @@ local DURATION_PARTIAL_FACTOR = 3.5;
 -- ------------------------------------------------
 -- Local Functions
 -- ------------------------------------------------
-
----
--- Shows a modal window that informs the player about something and only has
--- an okay button to be closed.
---
--- @param txt - The text to display on the modal.
--- @param centered - If set to true the modal will be centered (optional).
--- @param w - The width of the window (optional).
--- @param h - The height of the window (optional).
--- @param x - The x position of the modal (optional).
--- @param y - The y position of the modal (optional).
---
-local function showOkModal(txt, centered, w, h, x, y)
-    local x, y = x or 0, y or 0;
-    local w, h = w or 230, h or 120;
-    local core = getCore();
-
-    -- center the modal if necessary
-    if centered then
-        x = core:getScreenWidth() * 0.5 - w * 0.5;
-        y = core:getScreenHeight() * 0.5 - h * 0.5;
-    end
-
-    local modal = ISModalDialog:new(x, y, w, h, txt);
-    modal.backgroundColor = { r = 1.0, g = 0.0, b = 0.0, a = 0.5 };
-    modal:initialise();
-    modal:addToUIManager();
-end
 
 ---
 -- Converts a java arrayList to a lua table.
@@ -73,7 +46,7 @@ local function onUnpackBag(items, player, itemsInContainer, bag)
 
     -- Display a warning and abort the unpacking if the next item in the bag doesn't fit into the container.
     if container:getMaxWeight() < itemsInContainer[1]:getActualWeight() + container:getCapacityWeight() then
-        showOkModal(MODAL_WARNING_TEXT, true);
+        luautils.okModal(MODAL_WARNING_TEXT, true);
         return;
     end
 
