59c54a4942ce50f852fab926fb361dfe553c361a
Robert Machmer <robert.machmer@gmail.com>
2015-08-11 20:50:15 +0200

    Fix #11 - Bags are dropped correctly now when using Turn Tail

--- RMConvenientBags/media/lua/client/keyactions/DropBags.lua
+++ RMConvenientBags/media/lua/client/keyactions/DropBags.lua
@@ -1,14 +1,26 @@
 require'TimedActions/ISTimedActionQueue';
 
+function getBagToDrop(player)
+    local item = player:getPrimaryHandItem();
+    if item and item:getCategory() == 'Container' then
+        return item;
+    end
+    item = player:getSecondaryHandItem();
+    if item and item:getCategory() == 'Container' then
+        return item;
+    end
+    item = player:getClothingItem_Back();
+    if item and item:getCategory() == 'Container' then
+        return item;
+    end
+end
+
 local function onKeyPressed(key)
     if key == getCore():getKey('RMDropBags') then
         local player = getPlayer();
-        local item = player:getPrimaryHandItem()
-            or player:getSecondaryHandItem()
-            or player:getClothingItem_Back();
-
-        if item and item:getCategory() == 'Container' then
-            ISTimedActionQueue.add(TADropBags:new(getPlayer(), item));
+        local itemToDrop = getBagToDrop(player);
+        if itemToDrop then
+            ISTimedActionQueue.add(TADropBags:new(player, itemToDrop));
         end
     end
 end
